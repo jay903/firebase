5740a227fb861071aac8e1f8ee1d03f02f6eb486
jay <53287341+jay903@users.noreply.github.com>
2021-06-15 18:32:01 +0530

    for masoud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import fire from './config/Fire';
+import Login from './components/LoginRegister';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener(){
+    fire.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({user});
+      }else{
+        this.setState({user:null});
+      }
+    });
+  }
+
+  render(){
+    return (
+        <div>
+          {this.state.user ? (<Home />) : (<Login />)}
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config/Fire.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+var config = {
+    apiKey: "AIzaSyCoyhqEgs4K0s00hDPrKCqUD_IbKbjYdoA",
+    authDomain: "fir-92ba0.firebaseapp.com",
+    projectId: "fir-92ba0",
+    storageBucket: "fir-92ba0.appspot.com",
+    messagingSenderId: "172223286929",
+    appId: "1:172223286929:web:d86181a6d3b5e33fcfb930"
+  };
+  const fire = firebase.initializeApp(config);
+export default fire;
